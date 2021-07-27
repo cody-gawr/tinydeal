096a6d5c5f1d5ce2978537754e5e48ff846a6494
yhy940806 <yhy940806@163.com>
2021-07-26 21:10:46 -0400

    update allowed_domains and start_urls

--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -4,8 +4,8 @@ import scrapy
 
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
-    allowed_domains = ['www.tinydeal.com.hk/specials.html']
-    start_urls = ['http://www.tinydeal.com.hk/specials.html/']
+    allowed_domains = ['web.archive.org']
+    start_urls = ['https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html']
 
     def parse(self, response):
         pass
