d4ba3d01cb3a1dc78dc9d877be6dbb8966b1fd98
yhy940806 <yhy940806@163.com>
2021-07-26 21:59:38 -0400

    update User-Agent

--- tinydeal/settings.py
+++ tinydeal/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'tinydeal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tinydeal (+http://www.yourdomain.com)'
+# USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -5,7 +5,11 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['web.archive.org']
-    start_urls = ['https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
@@ -19,4 +23,6 @@ class SpecialOffersSpider(scrapy.Spider):
         next_page = response.xpath("//a[@class='nextPage']/@href").get()
 
         if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+        })
